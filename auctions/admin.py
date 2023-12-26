e4c8bab98a84ac8bf49817157e3bc8ea57baa3fd
glovastefan <glovastefan@gmail.com>
2023-12-26 01:11:54 +0000

    superuser and 2nd user created + all models added to admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from auctions.models import User
+from auctions.models import User, Categories, ActiveListing, Bid, Comment
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Categories)
+admin.site.register(ActiveListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
